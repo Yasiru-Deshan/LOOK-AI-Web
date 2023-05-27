3ea782aa4a9816a52f35443d595063b03098ca25
Yasiru-Deshan <yasirudeshan3@gmail.com>
2023-05-27 13:56:38 +0530

    media quaries added

--- lookai/src/components/Presentations/PresentationElements.js
+++ lookai/src/components/Presentations/PresentationElements.js
@@ -1,20 +1,20 @@
 import styled from "styled-components";
 
 export const ServicesContainer = styled.div`
-  height: 600px;
+  ${'' /* height: 600px; */}
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  
+  padding: 80px;
 
-  @media screen and (max-width: 768px) {
+  ${'' /* @media screen and (max-width: 768px) {
     height: 1100px;
   }
 
   @media screen and (max-width: 480px) {
     height: 1300px;
-  }
+  } */}
 `;
 export const ServicesWrapper = styled.div`
    max-width: 1000px;
--- lookai/src/components/Services/ServiceElements.js
+++ lookai/src/components/Services/ServiceElements.js
@@ -1,20 +1,21 @@
 import styled from 'styled-components'
 
 export const ServicesContainer = styled.div`
-  height: 1000px;
+  ${'' /* height: 1000px; */}
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   background: #37a7df;
+  padding:80px;
 
-  @media screen and (max-width: 768px) {
+  ${'' /* @media screen and (max-width: 768px) {
     height: 1100px;
   }
 
   @media screen and (max-width: 480px) {
     height: 1300px;
-  }
+  } */}
 `;
 export const ServicesWrapper = styled.div`
    max-width: 1000px;
